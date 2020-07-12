918c0f6922ae5f67e66bb102bff30d6a5f767c13
Pondddddddddd <nawaratnkw@gmail.com>
2020-07-12 13:34:00 +0700

    ready to deploy

--- app.py
+++ app.py
@@ -7,7 +7,25 @@ CORS(app)
 
 @app.route('/')
 def root():
-    return "Working"
+    return "yai_pond GOT7"
+
+@app.route('/new_one')
+def new_one_function():
+    username = request.args.get('name')
+    print(username)
+    return 'Hello!'+ username
+@app.route('/sum')
+def my_sum():
+    a = request.args.get('a')
+    a = int(a)
+    b = int (request.args.get('b'))
+    answer = a + b
+    return str(answer)
+
+@app.route('/mypage')
+def mypage():
+    username = request.args.get('name')
+    return render_template('home.html',name=username)
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
